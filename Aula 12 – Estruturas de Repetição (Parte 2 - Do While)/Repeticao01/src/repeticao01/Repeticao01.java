1f93d17dc852bbefcd3af161fafb3c36abea6f47
Wallisson Alves <wallissonlima008@gmail.com>
2023-12-26 17:15:11 -0300

    Versão final!
    Aberto para críticas caso meu programa tenha algum problema.
    Próximo passo é criar o mesmo programa com Jframe :smile:

--- Aula 12 – Estruturas de Repetição (Parte 2 - Do While)/Repeticao01/src/repeticao01/Repeticao01.java
+++ Aula 12 – Estruturas de Repetição (Parte 2 - Do While)/Repeticao01/src/repeticao01/Repeticao01.java
@@ -15,11 +15,11 @@ public class Repeticao01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int cc = 0;
+        int cc =1;
         do {
             System.out.println("cambalhota "+cc);
             cc++;
-        } while (cc<4);
+        } while (cc<5);
     }
     
 }
--- Desafios/CalculadoraBasica/src/calculadorabasica/CalculadoraBasica.java
+++ Desafios/CalculadoraBasica/src/calculadorabasica/CalculadoraBasica.java
@@ -11,66 +11,80 @@ public class CalculadoraBasica {
         Subtracao sub = new Subtracao();
         Multiplicacao mul = new Multiplicacao();
         Divisao div = new Divisao();
+        String continuar;
         
-        System.out.println("=====================");
-        System.out.println(" CALCULADORA BASICA ");
-        System.out.println("=====================");
-        System.out.println(" [ 1 ] ADICAO         ");
-        System.out.println(" [ 2 ] SUBTRACAO      ");
-        System.out.println(" [ 3 ] MULTIPLICACAO  ");
-        System.out.println(" [ 4 ] DIVISAO        ");
-        System.out.println("=====================");
-        System.out.print("Escola uma opcao: ");
-        int Opc = teclado.nextInt(); // Ler opção que usuário digitar na Tela
+        do {
+            System.out.println("=====================");
+            System.out.println(" CALCULADORA BASICA ");
+            System.out.println("=====================");
+            System.out.println(" [ 1 ] ADICAO         ");
+            System.out.println(" [ 2 ] SUBTRACAO      ");
+            System.out.println(" [ 3 ] MULTIPLICACAO  ");
+            System.out.println(" [ 4 ] DIVISAO        ");
+            System.out.println("=====================");
+            System.out.print("Escola uma opcao: ");
+            int Opc = teclado.nextInt(); // Ler opção que usuário digitar na Tela
 
-        switch(Opc){
-            case 1 -> {
-                System.out.print("Digite o primeiro valor: ");
-                double Esc1 = teclado.nextDouble();
-                System.out.print("Digite o segundo valor: ");
-                double Esc2 = teclado.nextDouble();
-                adi.soma(Esc1, Esc2);
-                System.out.print("A soma desses valores é: ");
-                System.out.println(adi.getResultado());
-                // deve retornar o procedimento de Soma
-                break;
+            switch(Opc){
+                case 1 -> {
+                    System.out.print("Digite o primeiro valor: ");
+                    double Esc1 = teclado.nextDouble();
+                    System.out.print("Digite o segundo valor: ");
+                    double Esc2 = teclado.nextDouble();
+                    adi.soma(Esc1, Esc2);
+                    System.out.println("=====================");
+                    System.out.println("     RESULTADO     ");
+                    System.out.println("=====================");
+                    System.out.println(Esc1+ " + " +Esc2+ " = " +adi.getResultado());
+                    // deve retornar o procedimento de Soma
+                    break;
+                }
+                case 2 -> {
+                    System.out.print("Digite o primeiro valor: ");
+                    double Esc1 = teclado.nextDouble();
+                    System.out.print("Digite o segundo valor: ");
+                    double Esc2 = teclado.nextDouble();
+                    sub.subt(Esc1, Esc2);
+                    System.out.println("=====================");
+                    System.out.println("     RESULTADO     ");
+                    System.out.println("=====================");
+                    System.out.println(Esc1+ " - " +Esc2+ " = " +sub.getResultado());
+                    // deve retornar o procedimento de Subtração
+                    break;
+                }
+                case 3 -> {
+                    System.out.print("Digite o primeiro valor: ");
+                    double Esc1 = teclado.nextDouble();
+                    System.out.print("Digite o segundo valor: ");
+                    double Esc2 = teclado.nextDouble();
+                    mul.Mult(Esc1, Esc2);
+                    System.out.println("=====================");
+                    System.out.println("     RESULTADO     ");
+                    System.out.println("=====================");
+                    System.out.println(Esc1+ " X " +Esc2+ " = " +mul.getMultiplicacao());
+                    // deve retornar o procedimento de Multiplicação
+                    break;
+                }
+                case 4 -> {
+                    System.out.print("Digite o primeiro valor: ");
+                    double Esc1 = teclado.nextDouble();
+                    System.out.print("Digite o segundo valor: ");
+                    double Esc2 = teclado.nextDouble();
+                    div.Divi(Esc1, Esc2);
+                    System.out.println("=====================");
+                    System.out.println("     RESULTADO     ");
+                    System.out.println("=====================");
+                    System.out.println(Esc1+ " / " +Esc2+ " = " +div.getDivisao());
+                    // deve retornar o procedimento de Divisão
+                    break;
+                }
+                default ->{
+                    System.out.println("Escolha uma opcao valida!");
+                }
             }
-            case 2 -> {
-                System.out.print("Digite o primeiro valor: ");
-                double Esc1 = teclado.nextDouble();
-                System.out.print("Digite o segundo valor: ");
-                double Esc2 = teclado.nextDouble();
-                sub.subt(Esc1, Esc2);
-                System.out.print("A subtracao desses valores é: ");
-                System.out.println(sub.getResultado());
-                // deve retornar o procedimento de Subtração
-                break;
-            }
-            case 3 -> {
-                System.out.print("Digite o primeiro valor: ");
-                double Esc1 = teclado.nextDouble();
-                System.out.print("Digite o segundo valor: ");
-                double Esc2 = teclado.nextDouble();
-                mul.Mult(Esc1, Esc2);
-                System.out.print("A multiplicacao desses valores: ");
-                System.out.println(mul.getMultiplicacao());
-                // deve retornar o procedimento de Multiplicação
-                break;
-            }
-            case 4 -> {
-                System.out.print("Digite o primeiro valor: ");
-                double Esc1 = teclado.nextDouble();
-                System.out.print("Digite o segundo valor: ");
-                double Esc2 = teclado.nextDouble();
-                div.Divi(Esc1, Esc2);
-                System.out.print("A divisao desses valores e: ");
-                System.out.println(div.getDivisao());
-                // deve retornar o procedimento de Divisão
-                break;
-            }
-            default ->{
-                System.out.println("Escolha uma opcao valida!");
-            }
-        }
+            System.out.print("Deseja realizar outra operacao? (S/N): ");
+            continuar = teclado.next();
+        } while (continuar.equals("S"));
+        System.out.println("Obrigado por utilizar meu programa :D");
     }    
 }
