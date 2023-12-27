8eb65001bc853a449d7e4f4deee1826a498e0b58
Wallisson Alves <wallissonlima008@gmail.com>
2023-12-27 15:48:55 -0300

    novo projeto baseado em algoritmos visualg

---
+++ Desafios/PosicaoNoAlfabeto/src/posicaonoalfabeto/PosicaoNoAlfabeto.java
@@ -0,0 +1,44 @@
+package posicaonoalfabeto;
+
+import java.util.Scanner;
+
+public class PosicaoNoAlfabeto {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner teclado = new Scanner (System.in);
+        String a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,letra;
+        
+        a = "A";
+        b = "B";
+        c = "C";
+        d = "D";
+        e = "E";
+        f = "F";
+        g = "G";
+        h = "H";
+        i = "I";
+        j = "J";
+        k = "K";
+        l = "L";
+        m = "M";
+        n = "N";
+        o = "O";
+        p = "P";
+        q = "Q";
+        r = "R";
+        s = "S";
+        t = "T";
+        u = "U";
+        v = "V";
+        w = "W";
+        x = "X";
+        y = "Y";
+        z = "Z";
+        
+        System.out.print("Digite uma letra MAIUSCULA para saber sua posicao no alfabeto: ");
+        letra = teclado.next();
+    }
+    
+}
